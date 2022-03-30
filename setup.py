cd4ecbaaa0e7768fa0eb6a9a02124520420df00e
Ekaterina Volkova <ekaterina.volkova@wirenboard.ru>
2022-03-30 18:36:03 +0300

    Rename folder, add 'console_scripts'

--- setup.py
+++ setup.py
@@ -2,12 +2,17 @@
 
 from setuptools import setup, find_packages
 
-setup(name          =   "modbus-utils-rpc",
-      version       =   "1.0",
-      author        =   "Ekaterina Volkova",
-      author_email  =   "ekaterina.volkova@wirenboard.ru",
-      description   =   "Wiren Board modbus utility using RPC",
-      url           =   "https://github.com/wirenboard/modbus-utils-rpc",
-      packages      =   find_packages(),
-      license	    =	'MIT'
-)
+setup(name="modbus-utils-rpc",
+      version="1.0",
+      author="Ekaterina Volkova",
+      author_email="ekaterina.volkova@wirenboard.ru",
+      description="Wiren Board modbus utility using RPC",
+      url="https://github.com/wirenboard/modbus-utils-rpc",
+      packages=find_packages(),
+      license='MIT',
+      entry_points={
+          'console_scripts': [
+              'modbus-utils-rpc = modbus_utils_rpc.main:main',
+          ],
+      }
+      )
