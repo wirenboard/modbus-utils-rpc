6e139b6c10894da2bf84d6f95a11e6be0ff3a063
Ekaterina Volkova <ekaterina.volkova@wirenboard.com>
2023-01-12 17:46:08 +0300

    Formatting

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
-setup(name="modbus-utils-rpc",
-      version="1.0",
-      author="Ekaterina Volkova",
-      author_email="ekaterina.volkova@wirenboard.ru",
-      description="Wiren Board modbus utility using RPC",
-      url="https://github.com/wirenboard/modbus-utils-rpc",
-      packages=['modbus_client_rpc','modbus_scanner_rpc'],
-      license='MIT'
-      )
+setup(
+    name="modbus-utils-rpc",
+    version="1.0",
+    author="Ekaterina Volkova",
+    author_email="ekaterina.volkova@wirenboard.ru",
+    description="Wiren Board modbus utility using RPC",
+    url="https://github.com/wirenboard/modbus-utils-rpc",
+    packages=["modbus_client_rpc", "modbus_scanner_rpc"],
+    license="MIT",
+)
