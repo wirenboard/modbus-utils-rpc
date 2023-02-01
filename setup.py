5d8fb7e7862cb3fa0f804b57e2f05bd340dff1b1
vdromanov <vdromanov@yahoo.com>
2023-02-01 17:14:41 +0300

    make black happier

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@
 
 from setuptools import setup, find_packages
 
-setup(name="modbus-utils-rpc",
-      version="1.0",
-      author="Ekaterina Volkova",
-      author_email="ekaterina.volkova@wirenboard.ru",
-      description="Wiren Board modbus utility using RPC",
-      url="https://github.com/wirenboard/modbus-utils-rpc",
-      packages=['modbus_client_rpc','modbus_scanner_rpc'],
-      license='MIT'
-      )
+setup(
+    name="modbus-utils-rpc",
+    version="1.0",
+    author="Ekaterina Volkova",
+    author_email="ekaterina.volkova@wirenboard.ru",
+    description="Wiren Board modbus utility using RPC",
+    url="https://github.com/wirenboard/modbus-utils-rpc",
+    packages=["modbus_client_rpc", "modbus_scanner_rpc"],
+    license="MIT",
+)
