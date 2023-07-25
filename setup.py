1dc1b4780b67389eab7b529781e366ae8ae88a01
Nikolay Korotkiy <sikmir@disroot.org>
2023-07-25 11:11:43 +0400

    Fix pkg-info (#17)

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python
 
-from setuptools import find_packages, setup
+from setuptools import setup
+
+
+def get_version():
+    with open("debian/changelog", "r", encoding="utf-8") as f:
+        return f.readline().split()[1][1:-1]
+
 
 setup(
     name="modbus-utils-rpc",
-    version="1.0",
+    version=get_version(),
     author="Ekaterina Volkova",
     author_email="ekaterina.volkova@wirenboard.ru",
+    maintainer="Wiren Board Team",
+    maintainer_email="info@wirenboard.com",
     description="Wiren Board modbus utility using RPC",
     url="https://github.com/wirenboard/modbus-utils-rpc",
     packages=["modbus_client_rpc", "modbus_scanner_rpc"],
